d380b1cb37814064ab8a046ad84f97f5e095521a
Oscar <oscarmeleroxd@gmail.com>
2023-07-09 10:29:03 +0200

    Initial commit

---
+++ src/main/java/sfkao/ptapi/PtapiApplication.java
@@ -0,0 +1,13 @@
+package sfkao.ptapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtapiApplication.class, args);
+	}
+
+}
