7b103416fe1031d615db35f577213122443a7323
Oscar <oscarmeleroxd@gmail.com>
2023-07-21 17:45:07 +0200

    Calculadas stats, skills y movimiento de los pokemon

--- src/main/java/sfkao/ptapi/service/SkillsServiceImpl1.java
+++ src/main/java/sfkao/ptapi/service/SkillsServiceImpl1.java
@@ -31,18 +31,64 @@ public class SkillsServiceImpl1 implements SkillService {
                 pokemon.getTipo2(),
                 formas,
                 calcularHabilidades(pokemon),
-                new Stat(  2, 1),
-                new Stat(   0, 3),
-                3
+                calcularStat(pokemon.getAtk(),pokemon.getSpa(),pokemon.getSpe()),
+                calcularStat(pokemon.getDef(), pokemon.getSpd(), pokemon.getHp()),
+                calcularMovimiento(pokemon.getSpe())
         );
     }
 
+    public static int calcularMovimiento(int spe){
+        if(spe >= 140)
+            return 5;
+        if(spe >= 110)
+            return 4;
+        if(spe >= 80)
+            return 3;
+        if(spe >= 50)
+            return 2;
+        return 1;
+    }
+
+    public static Stat calcularStat(int fis, int esp, int hpOSpe){
+        return new Stat(calcularTier(fis, hpOSpe), calcularTier(esp,hpOSpe));
+    }
+
+    public static int calcularTier(int stat, int aditivo){
+        if(stat >=170 || (stat >=140 && aditivo>= 85))
+            return 3;
+        if(stat >=140 || (stat >=110 && aditivo>= 85))
+            return 2;
+        if(stat >=110 || (stat >=95 && aditivo>= 85))
+            return 1;
+        return 0;
+    }
+
     public List<Skill> calcularHabilidades(Pokemon pokemon){
-        //TODO: Hacer
         ArrayList<Skill> skills = new ArrayList<>();
-        skills.add(new Skill("Vuelo","Puede volar","https://static.wikia.nocookie.net/leagueoflegendsoficial/images/8/8c/LOL_Logo.png/revision/latest?cb=20180119195439&path-prefix=es"));
-        skills.add(new Skill("Inmune veneno", "Es inmune al veneno","https://static.wikia.nocookie.net/leagueoflegendsoficial/images/8/8c/LOL_Logo.png/revision/latest?cb=20180119195439&path-prefix=es"));
+
+        if(esTipo(pokemon,"flying"))
+            skills.add(new Skill("Vuelo","Permite moverse por cualquier terreno","https://static.thenounproject.com/png/1211525-200.png"));
+        if(esTipo(pokemon,"water"))
+            skills.add(new Skill("Surf","Permite moverse por el agua","https://www.freeiconspng.com/thumbs/swimming-icon-png/sports-swimming-icon-png-1.png"));
+        if(esTipo(pokemon,"poison")||esTipo(pokemon,"steel"))
+            skills.add(new Skill("Inmunidad a veneno","Permite moverse por charcos de veneno sin envenenarse","https://static.wikia.nocookie.net/monster-legends-competitive/images/c/c0/Poison_Immunity.png/revision/latest/thumbnail/width/360/height/360?cb=20220306043936&path-prefix=fr"));
+        if(esTipo(pokemon,"rock"))
+            skills.add(new Skill("Atravesar montañas","Permite moverse por montañas y romper rocas","https://static.thenounproject.com/png/711504-200.png"));
+        if(esTipo(pokemon,"ice"))
+            skills.add(new Skill("Velocidad en la nieve","La reducción de movimiento en la nieve no le afecta","https://cdn-icons-png.flaticon.com/512/6661/6661508.png"));
+        if(esTipo(pokemon,"electric")||esTipo(pokemon,"ground"))
+            skills.add(new Skill("Inmunidad a campos electricos","Permite moverse por campos electricos sin paralizarse","https://cdn-icons-png.flaticon.com/512/6328/6328824.png"));
+        if(esTipo(pokemon,"fire"))
+            skills.add(new Skill("Caminar sobre lava","Permite caminar sobre la lava","https://cdn-icons-png.flaticon.com/512/1899/1899113.png"));
+
         return skills;
     }
 
+    private static boolean esTipo(Pokemon pokemon, String tipo){
+        if(pokemon.getTipo1().equals(tipo))
+            return true;
+        if(pokemon.getTipo2()==null)
+            return false;
+        return pokemon.getTipo2().equals(tipo);
+    }
 }
